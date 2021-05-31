75bbb2257d86540cb460aaea0913c821b19958d0
itscampa <mac.aerdna@gmail.com>
2021-06-01 01:03:25 +0200

    Added gnome config

--- init_setup.sh
+++ init_setup.sh
@@ -12,12 +12,39 @@ echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
 #alt tab only between current desktop windows
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 
+#al tab between windows and not applications
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab', '<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward  "['<Alt><Shift>Tab', '<Super><Shift>Tab']"
+
+#install albert
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+sudo apt update
+sudo apt install albert
+
+#gnome extensions:
+#https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+#https://extensions.gnome.org/extension/36/lock-keys/
+#https://extensions.gnome.org/extension/779/clipboard-indicator/
+#https://extensions.gnome.org/extension/444/cpu-freq/
+#https://extensions.gnome.org/extension/945/cpu-power-manager/
+#https://extensions.gnome.org/extension/7/removable-drive-menu/
+#https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
+#https://extensions.gnome.org/extension/1403/remove-alttab-delay/
+#https://extensions.gnome.org/extension/1482/remove-audio-device-selection-dialog/
+
+#looks:
+#https://extensions.gnome.org/extension/1011/dynamic-panel-transparency/
+
 #install bluetooth manager
 apt-get -y install blueman
 
 # install tlp and powertop for battery management
-apt-get -y install tlp
-apt-get -y install powertop
+#apt-get -y install tlp
+#apt-get -y install powertop
 
 #generate ssh keys (make sure you can use ssh-keygen)
 #should ask prompt to ask for pc name to give to key
